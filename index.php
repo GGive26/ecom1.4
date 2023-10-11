297b22ba97290224275e4cf8e708562b4e9ebe3d
GGive26 <precieudieudonne@gmail>
2023-10-11 15:48:05 -0400

    select the  column you want to see

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if($conn){
     echo"ERROR : Not connected to the $db database";
 }
 
+//recuperer une ligne dans user 
 $result1=mysqli_query($conn,"SELECT * FROM user WHERE id=2");
 //avec un tableau indexer
 $data1=mysqli_fetch_row($result1);
@@ -25,6 +26,7 @@ echo"<br>";
 echo"<br>";
 var_dump($data1);
 
+//recuperer une ligne dans user 
 $result2=mysqli_query($conn,"SELECT * FROM user WHERE id=1");
 //avec fetch tableau associatif
 $data2=mysqli_fetch_assoc($result2);
@@ -38,6 +40,21 @@ var_dump($result2);
 echo"<br>";
 echo"<br>";
 var_dump($data2);
+
+//recuperer une seule ligne mais en choisissant l'ordre des colonnes 
+$result3=mysqli_query($conn,"SELECT user_name,email,id  FROM user WHERE id=1");
+
+$data3=mysqli_fetch_assoc($result3);
+
+echo"<br>";
+echo"troisieme fetch";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+var_dump($result3);
+echo"<br>";
+echo"<br>";
+var_dump($data3);
 
 
 ?>
